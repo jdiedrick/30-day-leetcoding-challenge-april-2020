297588b765a8f8f64eff2cdd7f6ebde039f95be9
jo <jo@aquiet.life>
2020-04-01 10:20:51 -0400

    day 1 submission

---
+++ 01_single_number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+      
+        for num in nums[1:]:
+            nums[0] ^= num
+            
+        return nums[0]
---
+++ README.md
@@ -0,0 +1,3 @@
+# 30-Day LeetCoding Challenge
+
+Solutions for the 30-Day LeetCoding Challenge are in this repo!
